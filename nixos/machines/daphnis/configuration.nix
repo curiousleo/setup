f5353bc2e6b9392d23bb981036bc7ea88b4c1977
Leonhard Markert <curiousleo@users.noreply.github.com>
2019-11-02 22:36:58 +0100

    (daphnis) configure DHCP

--- nixos/machines/daphnis/configuration.nix
+++ nixos/machines/daphnis/configuration.nix
@@ -11,10 +11,8 @@
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
-  # TODO
-  #networking.useDHCP = false;
-  #networking.interfaces.enp0s25.useDHCP = true;
-  #networking.interfaces.wlp3s0.useDHCP = true;
+  networking.useDHCP = false;
+  networking.interfaces.wlp2s0.useDHCP = true;
 
   users.users.leo = {
     isNormalUser = true;
