3cc48a74787538e5852f6c2141727a3831fc8f45
Leonhard Markert <curiousleo@users.noreply.github.com>
2020-03-27 14:36:59 +0100

    (flatpak) install Discord, Skype

--- flatpak/install.sh
+++ flatpak/install.sh
@@ -16,6 +16,8 @@ flatpak --user remote-add --if-not-exists \
 	flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak --user --noninteractive update
 
+flatpak --user --noninteractive install flathub com.discordapp.Discord
+flatpak --user --noninteractive install flathub com.skype.Client
 flatpak --user --noninteractive install flathub com.slack.Slack
 flatpak --user --noninteractive install flathub com.spotify.Client
 flatpak --user --noninteractive install flathub com.valvesoftware.Steam
