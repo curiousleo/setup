e3b631e0a885b53e74fceba46f60fe284349cd5f
Leonhard Markert <curiousleo@users.noreply.github.com>
2020-04-08 16:41:27 +0200

    (wireguard) enable

--- nixos/common/configuration.nix
+++ nixos/common/configuration.nix
@@ -24,6 +24,9 @@
   # Enable NetworkManager.
   networking.networkmanager.enable = true;
 
+  # Enable Wireguard.
+  networking.wireguard.enable = true;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
