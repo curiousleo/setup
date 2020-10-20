ee2c7ca88dd36d4e7ee93d4fb9d91cfacde6fb59
Leonhard Markert <curiousleo@users.noreply.github.com>
2020-10-20 11:30:53 +0200

    (libvirtd) enable

--- nixos/common/configuration.nix
+++ nixos/common/configuration.nix
@@ -229,7 +229,7 @@ in
   #};
 
   virtualisation.docker.enable = true;
-  #virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd.enable = true;
   #virtualisation.virtualbox.host.enable = true;
   #virtualisation.virtualbox.host.enableExtensionPack = true;
 
