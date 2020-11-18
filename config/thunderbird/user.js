f0994475116a1861b27196d29a0fc6deb839380b
Leonhard Markert <curiousleo@users.noreply.github.com>
2020-11-18 16:46:07 +0100

    (thunderbird) commit view options

--- config/thunderbird/user.js
+++ config/thunderbird/user.js
@@ -2,3 +2,10 @@ user_pref("font.name.monospace.x-western", "Noto Sans Mono");
 user_pref("font.name.sans-serif.x-western", "Noto Sans");
 user_pref("font.name.serif.x-western", "Noto Serif");
 user_pref("font.size.monospace.x-western", 14);
+
+user_pref("mail.tabs.drawInTitlebar", true);
+user_pref("mail.inline_attachments", false);
+
+user_pref("mailnews.start_page.enabled", false);
+user_pref("mailnews.default_sort_order", 2);
+user_pref("mailnews.scroll_to_new_message", false);
