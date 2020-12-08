c3a62f63e4b0fe4a13350c89aa7c30d7e8620f8f
Leonhard Markert <curiousleo@users.noreply.github.com>
2020-12-08 22:41:39 +0100

    (users) not mutable

--- nixos/machines/orphne/configuration.nix
+++ nixos/machines/orphne/configuration.nix
@@ -16,13 +16,14 @@
   networking.interfaces.enp0s25.useDHCP = true;
   networking.interfaces.wlp3s0.useDHCP = true;
 
+  users.mutableUsers = false;
   users.users.leo = {
     isNormalUser = true;
     uid = 1000;
     hashedPassword =
       "$6$gbBlLam8Xck$12yL09PIVUkpQLpBz7JpbWDHsEb85T5D94Sot2aPRy/e3CV2B5KI0UbFq3P4Xgn2iHRQoORX/zYdVZlzhLXB1/";
     description = "Leonhard Markert";
-    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "docker" "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
   };
 
   services.restic.backups = {
